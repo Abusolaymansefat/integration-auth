7e89c82a1ddbea098c947a2dcd4c52711e92e9c5
Abusolay <sefat01625@gmail.com>
2025-04-29 22:16:56 +0600

    Auth  log in

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../../Contexts/AuthContext";
 
 const Login = () => {
+    const {signinUser} = use(AuthContext)
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password)
+
+
+        //login user
+        signinUser(email, password)
+        .then (result => {
+            console.log(result)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
   return (
     <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className="text-3xl text-center font-bold">Login now!</h1>
-        <form className="fieldset">
+        <form onSubmit={handleLogin} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" name="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name="password" className="input" placeholder="Password" />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
--- src/Contexts/AuthProvider.jsx
+++ src/Contexts/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase.init';
 
 const AuthProvider = ({children}) => {
@@ -8,8 +8,22 @@ const AuthProvider = ({children}) => {
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
+
+    const signinUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    onAuthStateChanged(auth, (currentUser) => {
+        if(currentUser){
+            console.log('has current user', currentUser)
+        }
+        else{
+            console.log('current user', createUser)
+        }
+    })
     const userInfo = {
-        createUser
+        createUser,
+        signinUser
     }
     return (
         <AuthContext value={userInfo}>
